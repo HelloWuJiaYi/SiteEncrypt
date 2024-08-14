f585fa12ced47130eed503a988b1f1e9a98ff8cb
WuJiaYi <30690400+HelloWuJiaYi@users.noreply.github.com>
2024-08-14 22:46:49 +0800

    Update Plugin.php

--- SiteEncrypt/Plugin.php
+++ SiteEncrypt/Plugin.php
@@ -4,7 +4,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 /**
  * 全站加密插件：
  * 
- * 这个插件允许你为整个博客设置一个访问密码，只有输入正确的密码后才能查看内容。
+ * <a target="_blank" href="https://github.com/HelloWuJiaYi/SiteEncrypt" rel="noopener noreferrer">SiteEncrypt</a> 允许你为整个博客设置一个访问密码，只有输入正确的密码后才能查看内容。
  * 
  * @package SiteEncrypt
  * @author 吴佳轶
@@ -14,31 +14,26 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 class SiteEncrypt_Plugin implements Typecho_Plugin_Interface
 {
-    // 激活插件
     public static function activate()
     {
         Typecho_Plugin::factory('Widget_Archive')->header = array('SiteEncrypt_Plugin', 'checkAccess');
         return _t('插件已激活，记得设置访问密码！');
     }
 
-    // 禁用插件
     public static function deactivate()
     {
     }
 
-    // 插件配置面板
     public static function config(Typecho_Widget_Helper_Form $form)
     {
         $password = new Typecho_Widget_Helper_Form_Element_Password('password', NULL, '', _t('访问密码'), _t('请输入访问博客的密码'));
         $form->addInput($password);
     }
 
-    // 个人用户的配置面板
     public static function personalConfig(Typecho_Widget_Helper_Form $form)
     {
     }
 
-    // 检查访问权限
     public static function checkAccess()
     {
         session_start();
@@ -46,7 +41,6 @@ class SiteEncrypt_Plugin implements Typecho_Plugin_Interface
         $password = $options->plugin('SiteEncrypt')->password;
         $errorMessage = '';
 
-        // 检查是否已通过认证
         if (!isset($_SESSION['site_encrypt_passed']) || $_SESSION['site_encrypt_passed'] !== true || (time() - $_SESSION['site_encrypt_time']) > 86400) {
             if (isset($_POST['site_encrypt_password'])) {
                 if ($_POST['site_encrypt_password'] === $password) {
@@ -58,7 +52,6 @@ class SiteEncrypt_Plugin implements Typecho_Plugin_Interface
             }
             
             if (!isset($_SESSION['site_encrypt_passed']) || $_SESSION['site_encrypt_passed'] !== true) {
-                // 显示密码输入表单
                 echo '<style>
                         .encrypt-form { max-width: 400px; margin: 100px auto; padding: 20px; border: 1px solid #ccc; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }
                         .encrypt-form label { display: block; margin-bottom: 8px; font-weight: bold; }
@@ -73,7 +66,7 @@ class SiteEncrypt_Plugin implements Typecho_Plugin_Interface
                         <input type="text" name="site_encrypt_password" required />
                         <input type="submit" value="提交" />
                       </form>';
-                exit; // 停止页面输出
+                exit; 
             }
         }
     }
